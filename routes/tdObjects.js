b0368586036ca639daabb775d31a6486a55c71de
Sachin Srinivasan <ssampathkumar@qwinix.io>
2018-10-30 21:35:18 +0530

    route

--- routes/tdObjects.js
+++ routes/tdObjects.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const Category = require('../models/Category');
+const TdObject = require('../models/TdObject');
 
-/* GET users listing. */
+// get categories and models
 router.get('/', (req, res, next) => {
   Category.find()
   .sort({ name: 1 })
@@ -17,6 +18,22 @@ router.get('/', (req, res, next) => {
     console.log(e)
     res
     .status(403)
+    .json({error: 'Something went wrong'});
+  });
+});
+
+// get model file and parse
+router.get('/parseModel/:id', (req, res, next) => {
+  TdObject.findOne({_id: req.params.id})
+  .then((model) => {
+    res
+    .status(200)
+    .json({ model });
+  })
+  .catch(e => {
+    console.log(e)
+    res
+    .status(404)
     .json({error: 'Something went wrong'});
   });
 });
